9796ad68a59492807900ed6b21b40a628f44fbc1
bogdanoviclj <33418394+bogdanoviclj@users.noreply.github.com>
2023-03-25 12:59:36 +0100

    Finished feature 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
+import { Route, Routes } from 'react-router';
+import { Layout } from './components/Layout/Layout';
+import { MainPage } from './components/MainPage/MainPage';
+import { Candidates } from './components/Candidates/Candidates';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path={'/'} element={<MainPage />} />
+      <Route path={'/'} element={<Layout />} >
+        <Route path={'/candidates'} element={<Candidates />} />
+      </Route>
+    </Routes>
   );
 }
 
