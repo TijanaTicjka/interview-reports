bf0cb5390779082debd7376eeb74dd0447b6c206
Lejla Čarovac-Mehmedović <samir_mehmedovic@live.com>
2023-04-01 11:25:53 +0200

    finished CandidateCard

---
+++ src/components/CandidateCard/CandidateCard.js
@@ -0,0 +1,15 @@
+
+
+import './CandidateCard.css';
+import { useNavigate } from 'react-router';
+
+export const CandidateCard = ({ id, avatar, name, email}) => {
+  const navigate = useNavigate()
+    return (
+      <div className='card' onClick={() => navigate(`/candidates/${id}`)}>
+        <img src={avatar} alt={`${name}'s avatar`} />
+        <div><h3>{name}</h3></div>
+        <div className='email'><p>{email}</p></div>
+      </div>
+    );
+  }
