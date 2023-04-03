3296f7f22bf13451f5b33afab29133654bd46195
TijanaTicjka <tijanamilenkovic13@gmail.com>
2023-04-03 17:52:00 +0200

    Made Reports page and conected with router plus mad e some style

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { Route, Routes } from 'react-router';
 import { Layout } from './components/Layout/Layout';
 import { SingleCandidate } from './components/SingleCandidate/SingleCandidate';
 import { Candidates } from './components/Candidates/Candidates.js';
+import { Reports } from './components/Reports/Reports';
 
 function App() {
   return (
     <Routes>
       <Route path={'/'} element={<Layout />} >
         <Route path={'/'} element={<Candidates/>}/>
-        <Route path={'/candidates'} element={<Candidates/>}/>
         <Route path={'/candidates/:id'} element={<SingleCandidate/>} />
+         <Route path={'/reports'} element={<Reports/>} />
       </Route>
     </Routes>
   );
